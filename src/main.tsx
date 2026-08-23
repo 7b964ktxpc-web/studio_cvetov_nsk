716fccb5c88b28d5f0570b3b36285825c7449a2c
7b964ktxpc-web <7b964ktxpc@privaterelay.appleid.com>
2026-08-23 16:18:22 +0700

    Update storefront hero copy

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import PublicBoutique2 from "./PublicBoutique2";
+import PublicBoutiqueCopy from "./PublicBoutiqueCopy";
 import "./styles.css";
 import "./theme.css";
 import "./boutique.css";
 import "./boutique2.css";
 import "./boutique-reference.css";
 
-function Root(){const path=window.location.pathname;return path.startsWith("/admin")?<App/>:<PublicBoutique2/>}
+function Root(){const path=window.location.pathname;return path.startsWith("/admin")?<App/>:<PublicBoutiqueCopy/>}
 createRoot(document.getElementById("root")!).render(<StrictMode><Root/></StrictMode>);
