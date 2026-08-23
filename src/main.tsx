636570b67693dacf6aa96ec77106701335f35c96
7b964ktxpc-web <7b964ktxpc@privaterelay.appleid.com>
2026-08-23 15:57:12 +0700

    Use resilient public boutique storefront

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import PublicBoutique from "./PublicBoutique";
+import PublicBoutique2 from "./PublicBoutique2";
 import "./styles.css";
 import "./theme.css";
 import "./boutique.css";
+import "./boutique2.css";
 
-function Root(){const path=window.location.pathname;return path.startsWith("/admin")?<App/>:<PublicBoutique/>}
+function Root(){const path=window.location.pathname;return path.startsWith("/admin")?<App/>:<PublicBoutique2/>}
 createRoot(document.getElementById("root")!).render(<StrictMode><Root/></StrictMode>);
