47c7522801e8893d92b155c2e8997a9e7d009d14
7b964ktxpc-web <7b964ktxpc@privaterelay.appleid.com>
2026-08-23 15:25:40 +0700

    Load refined florist typography

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./theme.css";
+import "./typography.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
