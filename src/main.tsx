bbfa4b2138a4c9cca9c20356f4b713a848070706
7b964ktxpc-web <7b964ktxpc@privaterelay.appleid.com>
2026-08-23 15:42:20 +0700

    Rebuild public florist storefront to match reference

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import PublicBoutique from "./PublicBoutique";
 import "./styles.css";
 import "./theme.css";
-import "./typography.css";
+import "./boutique.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
+function Root(){const path=window.location.pathname;return path.startsWith("/admin")?<App/>:<PublicBoutique/>}
+createRoot(document.getElementById("root")!).render(<StrictMode><Root/></StrictMode>);
